bfcf05ccf174b7b12f4ddd9a3803bbe272f790f7
Taranchuk <nfhfyxer@gmail.com>
2023-10-12 07:14:12 +0300

    mechanitor control

---
+++ Basic/1.4/Source/CompMechanitorControl.cs
@@ -0,0 +1,30 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Vehicles;
+using Verse;
+
+namespace VehicleMechanitorControl
+{
+    public class CompProperties_MechanitorControl : VehicleCompProperties
+    {
+        public int bandwidthGain;
+        public float mechControlRange;
+        public CompProperties_MechanitorControl()
+        {
+            this.compClass = typeof(CompMechanitorControl);
+        }
+    }
+    public class CompMechanitorControl : VehicleComp
+    {
+        public CompProperties_MechanitorControl Props => base.props as CompProperties_MechanitorControl;
+
+        public override IEnumerable<Gizmo> CompGetGizmosExtra()
+        {
+            foreach (Gizmo mechGizmo in MechanitorUtility.GetMechGizmos(this.Vehicle))
+            {
+                yield return mechGizmo;
+            }
+        }
+    }
+
+}
