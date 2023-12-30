7b749049e6d9b2cd003713bf43c63516214505a1
Taranchuk <nfhfyxer@gmail.com>
2023-12-31 00:04:08 +0200

    flight mode

---
+++ Basic/1.4/Source/taranchuk_flightcombat/VehiclePawn_UpdateRotation_Patch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using Vehicles;
+
+namespace taranchuk_flightcombat
+{
+    [HarmonyPatch(typeof(VehiclePawn), nameof(VehiclePawn.UpdateRotation))]
+    public static class VehiclePawn_UpdateRotation_Patch
+    {
+        public static bool Prefix(VehiclePawn __instance)
+        {
+            var comp = __instance.GetComp<CompFlightMode>();
+            if (comp != null && comp.flightMode)
+            {
+                comp.UpdateRotation();
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Basic/1.4/Source/taranchuk_flightcombat/VehiclePawn_VehicleGraphic_Patch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using Vehicles;
+
+namespace taranchuk_flightcombat
+{
+    [HarmonyPatch(typeof(VehiclePawn), nameof(VehiclePawn.VehicleGraphic), MethodType.Getter)]
+
+    public static class VehiclePawn_VehicleGraphic_Patch
+    {
+        public static bool Prefix(VehiclePawn __instance, ref Graphic_Vehicle __result)
+        {
+            var comp = __instance.GetComp<CompFlightMode>();
+            if (comp != null && comp.flightMode && comp.Props.flightGraphicData != null)
+            {
+                __result = comp.FlightGraphic;
+                return false;
+            }
+            return true;
+        }
+    }
+}
