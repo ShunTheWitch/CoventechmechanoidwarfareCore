246656d61e7b2bdd1ebf55b6050fcfead18b8063
Taranchuk <nfhfyxer@gmail.com>
2024-01-22 13:54:33 +0200

    fixes + chase mode

---
+++ Basic/1.4/Source/taranchuk_flightcombat/FlightCommand_Toggle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace taranchuk_flightcombat
+{
+    public class FlightCommand_Toggle : FlightCommand<Command_FlightMode>
+    {
+        public override Command_FlightMode GetCommand()
+        {
+            var command = new Command_FlightMode
+            {
+                defaultLabel = label,
+                defaultDesc = description,
+                icon = ContentFinder<Texture2D>.Get(texPath),
+            };
+            return command; 
+        }
+    }
+}
---
+++ Basic/1.4/Source/taranchuk_flightcombat/VehicleTurret_TurretDrawLocFor_Patch.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using SmashTools;
+using UnityEngine;
+using Vehicles;
+using Verse;
+
+namespace taranchuk_flightcombat
+{
+    [HarmonyPatch(typeof(VehicleTurret), nameof(VehicleTurret.TurretDrawLocFor))]
+    public static class VehicleTurret_TurretDrawLocFor_Patch
+    {
+        public static void Prefix(VehicleTurret __instance, ref Rot8 rot, out CompFlightMode __state)
+        {
+            var comp = __instance.vehicle?.GetComp<CompFlightMode>();
+            if (comp != null && __instance.vehicle.InFlightModeOrNonStandardAngle(comp))
+            {
+                __state = comp;
+                rot = Rot8.West;
+            }
+            else
+            {
+                __state = null;
+            }
+        }
+        public static void Postfix(ref Vector3 __result, CompFlightMode __state)
+        {
+            if (__state != null)
+            {
+                __result = __result.RotatedBy(__state.CurAngle);
+            }
+        }
+    }
+}
