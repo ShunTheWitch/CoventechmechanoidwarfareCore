5451258b06d78418b0f21f6532f4524823d19410
Taranchuk <nfhfyxer@gmail.com>
2024-02-09 14:23:19 +0200

    account for vehicle mass

---
+++ Basic/1.4/Source/taranchuk_flightcombat/Utils.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using Vehicles;
+using Verse;
+
+namespace taranchuk_flightcombat
+{
+    public static class Utils
+    {
+        public static float GetMass(this Pawn pawn)
+        {
+            if (pawn is VehiclePawn vehicle)
+            {
+                return vehicle.GetStatValue(VehicleStatDefOf.Mass);
+            }
+            return pawn.GetStatValue(StatDefOf.Mass);
+        }
+    }
+}
