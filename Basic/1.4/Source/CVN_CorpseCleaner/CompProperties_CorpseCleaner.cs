6108f2cba836288eb69612e0c7e2151bc3c28726
ShunTheWitch <62873836+ShunTheWitch@users.noreply.github.com>
2024-02-14 07:04:18 -0600

    Add files via upload

---
+++ Basic/1.4/Source/CVN_CorpseCleaner/CompProperties_CorpseCleaner.cs
@@ -0,0 +1,29 @@
+﻿using RimWorld;
+using Verse;
+
+namespace CVN_CorpseCleaner
+{
+    public class CompProperties_CorpseCleaner : CompProperties
+    {
+        public float radius = 5;
+
+        public int timeToComplete = 300;
+
+        public ThingDef thingToSpawn;
+
+        public ThingDef orbDef;
+
+        public int amountToSpawnPerCorpse;
+
+        public string gizmoName;
+
+        public string gizmoDesc;
+
+        public string gizmoIcon;
+
+        public CompProperties_CorpseCleaner()
+        {
+            compClass = typeof(CompCorpseCleaner);
+        }
+    }
+}
