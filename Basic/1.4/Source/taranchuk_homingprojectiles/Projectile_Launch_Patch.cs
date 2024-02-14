f8076b41aef2c3be521d6c00ae14f4c5cfc20b75
Taranchuk <nfhfyxer@gmail.com>
2024-02-14 15:20:14 +0200

    homing projectiles

---
+++ Basic/1.4/Source/taranchuk_homingprojectiles/Projectile_Launch_Patch.cs
@@ -0,0 +1,55 @@
+﻿using HarmonyLib;
+using System;
+using UnityEngine;
+using Verse;
+
+namespace taranchuk_homingprojectiles
+{
+    [HarmonyPatch(typeof(Projectile), "Launch", new Type[]
+    {
+        typeof(Thing),
+        typeof(Vector3),
+        typeof(LocalTargetInfo),
+        typeof(LocalTargetInfo),
+        typeof(ProjectileHitFlags),
+        typeof(bool),
+        typeof(Thing),
+        typeof(ThingDef)
+    })]
+    public static class Projectile_Launch_Patch
+    {
+
+        public static void Postfix(Projectile __instance, Thing launcher, Vector3 origin, ref LocalTargetInfo usedTarget,
+            LocalTargetInfo intendedTarget, bool preventFriendlyFire, Thing equipment, ThingDef targetCoverDef)
+        {
+            if (__instance.IsHomingProjectile(out var comp))
+            {
+                __instance.usedTarget = __instance.intendedTarget;
+                __instance.SetDestination(__instance.intendedTarget.CenterVector3 + comp.DispersionOffset);
+                comp.originLaunchCell = __instance.origin;
+                comp.launchTick = Find.TickManager.TicksGame;
+            }
+        }
+
+
+        public static void SetDestination(this Projectile projectile, Vector3 destination)
+        {
+            var projDestination = projectile.destination;
+            float distanceBetweenDestinations = Vector3.Distance(projDestination.Yto0(), destination.Yto0());
+            if (distanceBetweenDestinations >= 0.1f)
+            {
+                Vector3 origin = projectile.origin;
+                Vector3 newPos = new Vector3(projectile.ExactPosition.x, origin.y, projectile.ExactPosition.z);
+                projectile.origin = newPos;
+                projectile.destination = destination;
+                projectile.ticksToImpact = Mathf.CeilToInt(projectile.StartingTicksToImpact - 1);
+            }
+        }
+
+        public static bool IsHomingProjectile(this Projectile projectile, out CompHomingProjectile comp)
+        {
+            comp = projectile.GetComp<CompHomingProjectile>();
+            return comp != null;
+        }
+    }
+}
---
+++ Basic/1.4/Source/taranchuk_homingprojectiles/Projectile_Tick_Patch.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace taranchuk_homingprojectiles
+{
+    [HarmonyPatch(typeof(Projectile), nameof(Projectile.Tick))]
+    public static class Projectile_Tick_Patch
+    {
+        public static void Postfix(Projectile __instance)
+        {
+            if (__instance.IsHomingProjectile(out var comp))
+            {
+                if (comp.CanChangeTrajectory())
+                {
+                    //if (comp.RotateTowards(__instance.intendedTarget.CenterVector3, out var destination))
+                    //{
+                    //    __instance.SetDestination(destination);
+                    //}
+                    __instance.SetDestination(__instance.intendedTarget.CenterVector3);
+                }
+                if (__instance.Destroyed is false && comp.Props.lifetimeTicks > 0 & Find.TickManager.TicksGame - comp.launchTick > comp.Props.lifetimeTicks)
+                {
+                    __instance.ImpactSomething();
+                }
+            }
+        }
+    }
+}
