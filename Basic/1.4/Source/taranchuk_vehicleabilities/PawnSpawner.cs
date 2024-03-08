256bf338e9c7bb05db46f3bed001234214e62a11
Taranchuk <nfhfyxer@gmail.com>
2024-03-08 20:42:47 +0200

    burrow ability

---
+++ Basic/1.4/Source/taranchuk_vehicleabilities/PawnSpawner.cs
@@ -0,0 +1,32 @@
+﻿using Verse;
+
+namespace taranchuk_vehicleabilities
+{
+
+    public class PawnSpawner : ThingWithComps
+    {
+        public Pawn pawn;
+
+        public int spawnInTicks;
+
+        public override void Tick()
+        {
+            base.Tick();
+            spawnInTicks--;
+            if (spawnInTicks <= 0)
+            {
+                GenSpawn.Spawn(pawn, Position, Map);
+                Thing.allowDestroyNonDestroyable = true;
+                Destroy();
+                Thing.allowDestroyNonDestroyable = false;
+            }
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Deep.Look(ref pawn, "pawn");
+            Scribe_Values.Look(ref spawnInTicks, "spawnInTicks");
+        }
+    }
+}
