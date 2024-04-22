ea5f3578969a3300f0a67d877ecd1aaafc57680c
Taranchuk <nfhfyxer@gmail.com>
2024-04-22 18:03:41 +0300

    stashed arm awork

---
+++ 1.5/Source/taranchuk_mechanicalarms/MechanicalArmProps.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace taranchuk_mechanicalarms
+{
+    public class MechanicalArmProps
+    {
+        public Vector3 position;
+        public List<ArmSegmentProps> segments;
+    }
+}
---
+++ 1.5/Source/taranchuk_mechanicalarms/taranchuk_mechanicalarmsMod.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Linq;
+using UnityEngine;
+using Verse;
+
+namespace taranchuk_mechanicalarms
+{
+    public class taranchuk_mechanicalarmsMod : Mod
+    {
+        public taranchuk_mechanicalarmsMod(ModContentPack pack) : base(pack)
+        {
+			new Harmony("taranchuk_mechanicalarmsMod").PatchAll();
+        }
+    }
+
+
+}
