7b85a589f671f1f0fd6718e6a2b2d0302f9c79b7
Taranchuk <nfhfyxer@gmail.com>
2024-05-26 12:14:40 +0300

    update

---
+++ 1.5/Source/taranchuk_mechsuits/taranchuk_mechsuitsMod.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace taranchuk_mechsuits
+{
+    public class taranchuk_mechsuitsMod : Mod
+    {
+        public taranchuk_mechsuitsMod(ModContentPack pack) : base(pack)
+        {
+            new Harmony("taranchuk_mechsuitsMod").PatchAll();
+        }
+    }
+
+
+}
