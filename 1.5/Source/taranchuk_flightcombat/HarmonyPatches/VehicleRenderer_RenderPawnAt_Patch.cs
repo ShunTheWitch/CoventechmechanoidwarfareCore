93a7a8652a1a9f0b7d758d75aea36c8a4f4127b9
Taranchuk <nfhfyxer@gmail.com>
2024-08-01 17:22:14 +0300

    fixed flying vehicles not rotating

---
+++ 1.5/Source/taranchuk_flightcombat/HarmonyPatches/VehicleRenderer_RenderPawnAt_Patch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using SmashTools;
+using System;
+using UnityEngine;
+using Vehicles;
+
+namespace taranchuk_flightcombat
+{
+    [HotSwappable]
+    [HarmonyPatch(typeof(VehicleRenderer), nameof(VehicleRenderer.RenderPawnAt), new Type[] { typeof(Vector3), typeof(Rot8), typeof(float), typeof(bool) })]
+    public static class VehicleRenderer_RenderPawnAt_Patch
+    {
+        public static void Prefix(VehicleRenderer __instance, Vector3 drawLoc, Rot8 rot, ref float extraRotation, bool northSouthRotation)
+        {
+            var comp = __instance.vehicle.GetComp<CompFlightMode>();
+            if (comp != null && comp.InAir)
+            {
+                extraRotation += comp.AngleAdjusted(comp.CurAngle + comp.FlightAngleOffset);
+            }
+        }
+    }
+}
