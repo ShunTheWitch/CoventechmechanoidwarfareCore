3cf9be1bc218a8d99fcc599b2aa1b5f3ad39c052
Taranchuk <nfhfyxer@gmail.com>
2025-12-11 01:10:49 +0200

    fix

--- 1.6/Source/taranchuk_flightcombat/HarmonyPatches/DamageWorker_ExplosionDamageThing_Patch.cs
+++ 1.6/Source/taranchuk_flightcombat/HarmonyPatches/DamageWorker_ExplosionDamageThing_Patch.cs
@@ -12,24 +12,37 @@ namespace taranchuk_flightcombat
         private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator ilGen)
         {
             bool found = false;
-            var takeDamage = AccessTools.Method(typeof(Thing), nameof(Thing.TakeDamage));
             var applyEffects = AccessTools.Method(typeof(DamageWorker_ExplosionDamageThing_Patch), nameof(ApplyEffects));
             var codes = instructions.ToList();
+
             for (var i = 0; i < codes.Count; i++)
             {
-                if (!found && codes[i + 1].Calls(takeDamage))
+                if (!found && i + 1 < codes.Count &&
+                    (codes[i + 1].opcode == OpCodes.Callvirt || codes[i + 1].opcode == OpCodes.Call))
                 {
-                    found = true;
-                    yield return new CodeInstruction(OpCodes.Ldarg_1);
-                    yield return new CodeInstruction(OpCodes.Ldarg_2);
-                    yield return new CodeInstruction(OpCodes.Ldloc, 1);
-                    yield return new CodeInstruction(OpCodes.Call, applyEffects);
+                    var method = codes[i + 1].operand as System.Reflection.MethodInfo;
+                    if (method != null &&
+                        method.ReturnType.Name == "DamageResult" &&
+                        method.GetParameters().Length > 0 &&
+                        method.GetParameters()[0].ParameterType.Name == "DamageInfo")
+                    {
+                        found = true;
+                        yield return new CodeInstruction(OpCodes.Ldarg_1);
+                        yield return new CodeInstruction(OpCodes.Ldarg_2);
+                        yield return new CodeInstruction(OpCodes.Ldloca_S, 1);
+                        yield return new CodeInstruction(OpCodes.Call, applyEffects);
+                    }
                 }
                 yield return codes[i];
             }
+
+            if (!found)
+            {
+                Log.Error("taranchuk_flightcombat: Failed to find TakeDamage call in ExplosionDamageThing!");
+            }
         }
 
-        public static void ApplyEffects(Explosion explosion, Thing hitThing, DamageInfo damageInfo)
+        public static void ApplyEffects(Explosion explosion, Thing hitThing, ref DamageInfo damageInfo)
         {
             if (hitThing != null)
             {
