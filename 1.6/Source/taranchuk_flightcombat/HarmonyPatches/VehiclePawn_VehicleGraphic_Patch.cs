34cae6bc879da07ad9227b0ad1a2358a6aedfa41
Taranchuk <nfhfyxer@gmail.com>
2025-12-11 00:35:16 +0200

    grey texture fix

--- 1.6/Source/taranchuk_flightcombat/HarmonyPatches/VehiclePawn_VehicleGraphic_Patch.cs
+++ 1.6/Source/taranchuk_flightcombat/HarmonyPatches/VehiclePawn_VehicleGraphic_Patch.cs
@@ -7,7 +7,7 @@ namespace taranchuk_flightcombat
     [HarmonyPatch(typeof(VehiclePawn), nameof(VehiclePawn.VehicleGraphic), MethodType.Getter)]
     public static class VehiclePawn_VehicleGraphic_Patch
     {
-        public static bool Prefix(VehiclePawn __instance, ref Graphic_Vehicle __result)
+        public static void Postfix(VehiclePawn __instance, ref Graphic_Vehicle __result)
         {
             var comp = __instance.GetComp<CompFlightMode>();
             if (comp != null && comp.InAir && comp.Props.flightGraphicData != null)
@@ -16,10 +16,8 @@ namespace taranchuk_flightcombat
                 if (flightGraphic != null)
                 {
                     __result = flightGraphic;
-                    return false;
                 }
             }
-            return true;
         }
     }
 }
