6407fdc7af520b44c98727d893dc0c70bef13744
Taranchuk <nfhfyxer@gmail.com>
2025-12-14 01:32:22 +0200

    fix

---
+++ 1.6/Source/taranchuk_flightcombat/HarmonyPatches/VehiclePawn_Angle_Patch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using Vehicles;
+
+namespace taranchuk_flightcombat
+{
+    [HarmonyPatch(typeof(VehiclePawn), nameof(VehiclePawn.Angle), MethodType.Getter)]
+    public static class VehiclePawn_Angle_Patch
+    {
+        [HarmonyPriority(Priority.Last)]
+        public static bool Prefix(VehiclePawn __instance, ref float __result)
+        {
+            var comp = __instance.GetComp<CompFlightMode>();
+            if (comp != null && comp.InAir)
+            {
+                __result = __instance.angle;
+                return false;
+            }
+            return true;
+        }
+    }
+}
