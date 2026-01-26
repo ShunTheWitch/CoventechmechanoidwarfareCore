afa8f2bc330357d4ff75d0d8916f0f78c815866a
Taranchuk <nfhfyxer@gmail.com>
2026-01-26 12:36:01 +0200

    patch fix

--- 1.6/Source/taranchuk_homingprojectiles/Projectile_SetTrueOrigin.cs
+++ 1.6/Source/taranchuk_homingprojectiles/Projectile_SetTrueOrigin.cs
@@ -1,4 +1,4 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -18,34 +18,35 @@ namespace taranchuk_homingprojectiles
             yield return AccessTools.Method(typeof(Projectile), "ImpactSomething");
         }
 
-        public static MethodInfo InterceptChanceFactorFromDistanceInfo
-            = AccessTools.Method(typeof(Verse.VerbUtility), "InterceptChanceFactorFromDistance");
+        public static MethodInfo InterceptChanceFactorFromDistanceInfo = typeof(Verse.VerbUtility).Method("InterceptChanceFactorFromDistance");
+        public static FieldInfo Projectile_origin = typeof(Projectile).Field("origin");
 
         public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codeInstructions)
         {
             var codes = codeInstructions.ToList();
-            var field = AccessTools.Field(typeof(Projectile), nameof(Projectile.origin));
             for (var i = 0; i < codes.Count; i++)
             {
                 var codeInstruction = codes[i];
-                if (codes.Count - 3 > i && codeInstruction.LoadsField(field) && codes[i + 2].Calls(InterceptChanceFactorFromDistanceInfo))
+
+                bool shouldPatch = codeInstruction.LoadsField(Projectile_origin) &&
+                                  codes.Skip(i + 1).Any(c => c.Calls(InterceptChanceFactorFromDistanceInfo));
+                yield return codeInstruction;
+                if (shouldPatch)
                 {
-                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Projectile_SetTrueOrigin), nameof(GetTrueOrigin)));
-                }
-                else
-                {
-                    yield return codeInstruction;
+                    yield return new CodeInstruction(OpCodes.Ldarg_0);
+                    yield return new CodeInstruction(OpCodes.Call,
+                        AccessTools.Method(typeof(Projectile_SetTrueOrigin), nameof(GetTrueOrigin)));
                 }
             }
         }
 
-        public static Vector3 GetTrueOrigin(Projectile projectile)
+        public static Vector3 GetTrueOrigin(Vector3 origin, Projectile projectile)
         {
             if (projectile.IsHomingProjectile(out var comp))
             {
                 return comp.originLaunchCell;
             }
-            return projectile.origin;
+            return origin;
         }
     }
 }
